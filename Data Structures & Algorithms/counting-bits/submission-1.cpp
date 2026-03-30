27fcdab16472761dbe1cb207b767d8ccadb68814
Abhishek kumar <ak95722352@gmail.com>
2026-03-30 23:25:32 +0530

    Add: counting-bits - submission-1

---
+++ Data Structures & Algorithms/counting-bits/submission-1.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int>v(n+1,0);
+        for(int i=1; i<=n; i++){
+            v[i]=(i&1) + v[i/2];
+            //v[i] = v[i & (i - 1)] + 1;
+        }
+        return v;
+    }
+};
